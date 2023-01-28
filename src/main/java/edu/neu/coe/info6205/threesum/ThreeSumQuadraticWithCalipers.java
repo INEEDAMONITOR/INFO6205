713b51dc2bc5e331605797cbf0f94f88aca9b465
INEEDAMONITOR <joel.yuxi@gmail.com>
2023-01-28 17:49:48 -0500

    Implement ThreeSumQuadraticWithCaplipers getTriples

--- src/main/java/edu/neu/coe/info6205/threesum/ThreeSumQuadraticWithCalipers.java
+++ src/main/java/edu/neu/coe/info6205/threesum/ThreeSumQuadraticWithCalipers.java
@@ -49,7 +49,23 @@ public class ThreeSumQuadraticWithCalipers implements ThreeSum {
     public static List<Triple> calipers(int[] a, int i, Function<Triple, Integer> function) {
         List<Triple> triples = new ArrayList<>();
         // FIXME : use function to qualify triples and to navigate otherwise.
-        // END 
+        // The first
+        int left = i + 1, right = a.length - 1;
+        while (left < right) {
+            int sum = a[i] + a[left] + a[right];
+            if (sum == 0) {
+                triples.add(new Triple(i, left, right));
+                left++;
+                right--;
+                continue;
+            }
+            if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        // END
         return triples;
     }
 
